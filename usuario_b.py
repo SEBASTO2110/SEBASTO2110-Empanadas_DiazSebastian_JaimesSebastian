2fce8b7816bb8ead25d9ac5a642e75e044657ef0
sebastianjaimesr <jsrolon01@gmail.com>
2026-03-05 15:48:50 -0500

    feat:haciendo funciones

--- usuario_b.py
+++ usuario_b.py
@@ -11,4 +11,21 @@ def cargar_empanadas():
 def guardar_empanadas(empanadas):
     with open(ARCHIVO, "w") as archivo:
         json.dump(empanadas, archivo, indent=4)
-        
+
+def editar_empanada():
+    empanadas = cargar_empanadas()
+
+    for i, emp in enumerate(empanadas, start=1):
+        print(f"{i}. {emp['nombre']} - ${emp['precio']}")
+
+    opcion = int(input("Seleccione la empanada a editar: ")) - 1
+
+    nuevo_nombre = input("Nuevo nombre: ")
+    nuevo_precio = input("Nuevo precio: ")
+
+    empanadas[opcion]["nombre"] = nuevo_nombre
+    empanadas[opcion]["precio"] = nuevo_precio
+
+    guardar_empanadas(empanadas)
+
+    print("Empanada actualizada.")
