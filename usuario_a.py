b39794d7a2eccca9684935cc139091074016c015
SEBASTO2110 <sebasto2110@gmail.com>
2026-03-05 15:49:49 -0500

    feat: se crea la funcion agregar_empanada con sus variables

--- usuario_a.py
+++ usuario_a.py
@@ -11,7 +11,7 @@ def cargar_empanadas():
 
 def guardar_empanadas(empanadas):
     with open(ARCHIVO, "w") as archivo:
-        json.dump(empanadas, archivo, indent=4)}
+        json.dump(empanadas, archivo, indent=4)
 
 def listar_empanadas():
     empanadas = cargar_empanadas()
@@ -23,3 +23,9 @@ def listar_empanadas():
         
     for i, emp in enumerate(empanadas, start=1):
         print(f"{i}. {emp['nombre']} - ${emp['precio']}")        
+
+def agregar_empanada():
+    empanadas = cargar_empanadas()
+
+    nombre = input("Nombre de la empanada: ")
+    precio = input("Precio: ")
